779cea17f05f1e23dbc6914995b095ddece94a26
Reid Moffat <18rem8@queensu.ca>
2022-01-29 22:24:54 -0500

    fixed axois error

--- src/components/newalert.js
+++ src/components/newalert.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import getPrice from '../finnhub.js'
-import { newAlert } from '../firebase.js';
+import {newAlert} from '../firebase.js';
+import getPrice from "../finnhub";
 
 class NewAlert extends React.Component {
     constructor(props) {
@@ -15,50 +15,50 @@ class NewAlert extends React.Component {
         this.targetChange = this.targetChange.bind(this);
     }
 
-     handleSubmit = async (event) => {
+    handleSubmit = async (event) => {
         event.preventDefault();
         // SUBMIT BUTTON EVENT HANDLER
         console.log(this.state.stock)
-        try{
-            var price = await getPrice(this.state.stock)
-        }
-        catch(e){
+        let price;
+        try {
+            price = await getPrice(this.state.stock);
+        } catch (e) {
             console.log(e)
         }
-        if (price === 0){
+        if (price <= 0) {
             console.log("error")
             return
         }
         console.log(price)
-        var today = new Date();
-        var dd = String(today.getDate()).padStart(2, '0');
-        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-        var yyyy = today.getFullYear();
+        let today = new Date();
+        const dd = String(today.getDate()).padStart(2, '0');
+        const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        const yyyy = today.getFullYear();
 
         today = mm + '/' + dd + '/' + yyyy;
         await newAlert(this.state.stock, price, this.state.target, today)
 
     }
 
-    stockChange(event){
+    stockChange(event) {
         console.log(event.target.value)
         var temp = event.target.value
         this.setState({stock: temp})
     }
 
-    targetChange(event){
+    targetChange(event) {
         this.setState({target: event.target.value})
     }
 
     render() {
         return (
             <>
-            <input type="text" name="stock" onChange={this.stockChange}/>
-            <input type="number" name="target" onChange={this.targetChange}/>
-            <input type="submit" value="Add New Stock Alert!" onClick={this.handleSubmit}/>
-        </>
+                <input type="text" name="stock" onChange={this.stockChange}/>
+                <input type="number" name="target" onChange={this.targetChange}/>
+                <input type="submit" value="Add New Stock Alert!" onClick={this.handleSubmit}/>
+            </>
         );
     }
-  }
+}
 
 export default NewAlert
--- src/finnhub.js
+++ src/finnhub.js
@@ -4,9 +4,9 @@ const axios = require("axios").default;
  * Returns the current price of a stock given its ticker
  * If the stock price cannot be obtained, an error value is returned
  */
- export const getPrice = async (ticker) => {
+const getPrice = async (ticker) => {
     // url for stock data
-    
+
     const data = `https://finnhub.io/api/v1/quote?symbol=${ticker}&token=c7qcivqad3i9it665jk0`;
     console.log(ticker)
     // Get and return the stock price
@@ -14,12 +14,12 @@ const axios = require("axios").default;
     await axios.request({url: data}).then((response) => {
         console.log(response)
         resp = response.data["c"];
-        if(resp == "0") throw "Stock Ticker Invalid";
-        if(response.data["status_code"] == "204") throw "Too Many API Calls";
+        if (resp == "0") throw "Stock Ticker Invalid";
+        if (response.data["status_code"] == "204") throw "Too Many API Calls";
     }).catch(error => {
         throw "Error getting stock price: " + error.message;
     });
     return resp;
 };
 
-export default axios
+export default getPrice;
