da746396c7c12dcad085be20aa9cbba8cc0aa23a
Reid Moffat <18rem8@queensu.ca>
2023-09-10 22:16:34 -0400

    firebase config

--- src/backend/firebase.js
+++ src/backend/firebase.js
@@ -1,18 +1,28 @@
+// TODO remove
 import firebase from 'firebase/compat/app';
 import {addDoc, collection, deleteDoc, doc, getDocs, getFirestore, setDoc, updateDoc} from "firebase/firestore"
 import {createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword} from "firebase/auth";
 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import 'firebase/functions';
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+    apiKey: "AIzaSyAoggCGTMuz7vSAOyfi1F16EVn07j1Ou4A",
+    authDomain: "stock-alert-2042e.firebaseapp.com",
+    projectId: "stock-alert-2042e",
+    storageBucket: "stock-alert-2042e.appspot.com",
+    messagingSenderId: "811152286829",
+    appId: "1:811152286829:web:814cff3192cafeda0ecb64"
 };
 
-const app = firebase.initializeApp(firebaseConfig)
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const db = getFirestore();
 const auth = getAuth(app);
 
