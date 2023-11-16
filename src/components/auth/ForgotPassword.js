dff6b9e4214138af7028b530cd94e26af1611f78
Reid Moffat <18rem8@queensu.ca>
2023-11-15 20:57:39 -0500

    reset message

--- src/components/auth/ForgotPassword.js
+++ src/components/auth/ForgotPassword.js
@@ -1,20 +1,37 @@
 import React from 'react';
-import { forgotPassword, signIn } from "../../backend/endpoints";
+import { Vortex } from 'react-loader-spinner';
+import { getFunctions, httpsCallable } from "firebase/functions";
 
 class ForgotPassword extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            loading: false,
+            message: '',
+            isError: false
+        };
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleSubmit = (event) => {
-        forgotPassword(event.target.email.value)
-            .then(() => {})
-            .catch(() => {});
+    handleSubmit = async (event) => {
+        await this.setState({ loading: true });
+        httpsCallable(getFunctions(), 'sendPasswordResetEmail')({ email: event.target.email.value })
+            .then(async () => {
+                await this.setState({ loading: false, message: 'Please check your email', isError: false });
+            })
+            .catch(async () => {
+                await this.setState({ loading: false, message: 'Error sending email; please try again later', isError: true });
+            });
 
         event.preventDefault();
+    }
+
+    renderMessage = () => {
+        if (!this.state.message) return <></>;
+
+        const textStyle = this.state.isError ? { 'color': 'red' } : { 'color': 'green' };
+        return <><br/><br/><text style={textStyle}>{this.state.message}</text></>;
     }
 
     render() {
@@ -26,6 +43,8 @@ class ForgotPassword extends React.Component {
                     <input type="text" class="field" name="email"/>
                     <br/>
                     <input type="submit" class="form-btn btn" value="Send link"/>
+                    {this.renderMessage()}
+                    {this.state.loading && <Vortex colors={['#eea76a', '#b84410', '#eea76a', '#b84410', '#b84410', '#eea76a']} width={50} height={50}/>}
                 </form>
             </div>
         );
