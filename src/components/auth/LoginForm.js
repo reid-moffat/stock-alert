8beeda84c18012a1df3d2a71f31d805a5dfb8805
Reid Moffat <18rem8@queensu.ca>
2023-11-16 19:55:37 -0500

    input msg

--- src/components/auth/LoginForm.js
+++ src/components/auth/LoginForm.js
@@ -1,18 +1,44 @@
 import React from 'react';
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../backend/firebase";
+import SpinningLoader from "../Visuals/SpinningLoader";
 
 class LoginForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            loading: false,
+            message: ''
+        };
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleSubmit = (event) => {
+    handleSubmit = async (event) => {
+        event.preventDefault();
+
+        if (event.target.email.value) {
+            ;
+        }
+        if (!event.target.password.value) {
+            this.setState({ message: 'Please enter a password' });
+            return;
+        }
+        if (event.target.password.value.length < 6) {
+            ;
+        }
+
+        await this.setState({ loading: true });
         signInWithEmailAndPassword(auth, event.target.email.value, event.target.password.value)
             .then(() => this.props.onLogin())
-        event.preventDefault();
+            .catch(async (err) => {
+                console.log(`Error code: ${err.code} Message: ${err.message} All: ${JSON.stringify(err, null, 4)}`);
+                await this.setState({ loading: false });
+            });
+    }
+
+    renderMessage = () => {
+        if (!this.state.message) return <></>;
+        return <><br/><br/><text style={{ 'color': 'red' }}>{this.state.message}</text></>;
     }
 
     render() {
@@ -31,9 +57,11 @@ class LoginForm extends React.Component {
                             </button>
                         </span>
                         <br/>
-                        <input type="password" class="field" name="password"/>
+                        <input type="password" class="field" name="password" pattern="^.{6,}$" title="Please enter a valid password (6+ characters)" required/>
                         <br/>
                         <input type="submit" class="form-btn btn" value="Login"/>
+                        <SpinningLoader loading={this.state.loading}/>
+                        {this.renderMessage()}
                     </form>
                 </div>
                 <br/>
